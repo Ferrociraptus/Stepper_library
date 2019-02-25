8a638e888470ef627dd4212ddca84b0e5036bac0
Alexeev Arseniy <ars@alexeevs.info>
2019-02-25 22:01:38 +0300

    Add keywords.txt file. Write functions for Stepper class.

--- src/Stepper.cpp
+++ src/Stepper.cpp
@@ -1,7 +1,242 @@
-#include "Stepper.h"
+#include "STEPPER.h"
 #include <Arduino.h>
 
+//Constructor:
+Stepper::Stepper(byte stpPin, byte dirPin, byte enaPin = NONE){
+  this->stpPin = stpPin;
+  this->dirPin = dirPin;
+  this->enaPin = enaPin;
 
-Stepper::Stepper(){
-  
+  if (enaPin != NONE){
+    pinMode(enaPin, OUTPUT);
+    digitalWrite(enaPin, LOW);
+  }
+  pinMode(dirPin, OUTPUT);
+  digitalWrite(dirPin, LOW);
+
+  pinMode(stpPin, OUTPUT);
+  digitalWrite(stpPin, LOW);
 }
+//End of Constructor;
+
+
+//Set settings:
+
+  //Settings of rotation for stepper motor:
+  void setmmPerTurn(float mmPerTurn){
+    this->mmPerTurn = mmPerTurn;
+  }
+
+  void setAcceleration(int acceleration){
+    this->acceleration = acceleration;
+  }
+
+  void setStepDivision(byte division){
+    this->stepDivision = division;
+  }
+
+  void setAnglePerStep(float angle){
+    this->anglePerStep = angle;
+  }
+
+  void setStartSpeed(float speed){
+    this->startSpeed = speed;
+  }
+
+  void setMaxSpeed(float speed){
+    this->maxSpeed = speed;
+  }
+  //End of settings of rotation for stepper motor;
+
+  //Settings of operation mode:
+  void setMotorMode(String Mode, String enaLvl = "LOW"){
+    Mode = Mode.toUpperCase();
+    if (Mode = "ON"){
+      if (reverseMod = false) digitalWrite(enaPin, HIGH);
+      if (reverseMod = true) digitalWrite(enaPin, LOW);
+    }
+    else{
+      if (reverseMod = false) digitalWrite(enaPin, LOW);
+      if (reverseMod = true) digitalWrite(enaPin, HIGH);
+    }
+  }
+
+  void reverse(){
+    this->reverseMod = !reverseMod;
+  }
+
+  void setImpulseTime(byte impulseTime){
+    this->impulseTime = impulseTime;
+  }
+  //End of Settings of operation mode;
+
+
+
+  //Settings of position set:
+  void setPosition(float position = 0){
+    this->position = position;
+  }
+
+  void setEndBorder(float endBorderPosition){
+      this->endBorder = endBorderPosition;
+  }
+
+  void setBeginBorder(float beginBorderPosition);{
+    this->beginBorder = beginBorderPosition;
+  }
+  //End settings of position set;
+
+//End of set-settings;
+
+
+
+//Metods for work with stepper motor:
+
+  //Position metods:
+  void  position(float Position){
+    this->position = changePosition(Position - position);
+  }
+
+  float changePosition(float changePos){
+    this->stepsPerMM = (360.0 / anglePerStep) * stepDivision) / mmPerTurn;
+    this->position = poition + changePos;
+
+    if (changePos < 0){
+        if (reverseMod = false) digitalWrite(dirPin, HIGH);
+        if (reverseMod = true) digitalWrite(dirPin, LOW);
+
+        changePos = -changePos;
+        if (beginBorder != NONE && position < beginBorder){
+          changePos = changePos - (position - beginBorder);
+          this->position = beginBorder;
+        }
+    }
+    else{
+        if (reverseMod = false) digitalWrite(dirPin, LOW);
+        if (reverseMod = true) digitalWrite(dirPin, HIGH);
+
+        if (endBorder != NONE && position > endBorder){
+          changePos = changePos - (position - endBorder);
+          this->position = endBorder;
+        }
+    }
+
+      float accelerationWay = ((maxSpeed * maxSpeed) - (startSpeed * startSpeed)) / (2 * acceleration)
+      if (accelerationWay * 2 > changePos) accelerationWay = changePos / 2;
+
+      float accelerationWayBuffer = accelerationWay;
+      int stepTime = 0;
+      float speed = startSpeed;
+
+      while (accelerationWayBuffer >= 0){
+          if(speed < maxSpeed){
+            speed = speed + (acceleration * stepTime);
+            if(speed > maxSpeed) speed = maxSpeed;
+          }
+          stepTime = ()(1000000 / speed) / stepsPerMM) - impulseTime;
+          accelerationWayBuffer = accelerationWayBuffer - (speed * stepTime);
+          digitalWrite(stpPin, HIGH);
+          delayMicroseconds(impulseTime);
+          digitalWrite(stpPin, LOW);
+          delayMicroseconds(stepTime);
+      }
+
+      accelerationWayBuffer = changePos - (2 * accelerationWay);
+
+      while (accelerationWayBuffer >= 0){
+          accelerationWayBuffer = accelerationWayBuffer - (speed * stepTime);
+          digitalWrite(stpPin, HIGH);
+          delayMicroseconds(impulseTime);
+          digitalWrite(stpPin, LOW);
+          delayMicroseconds(stepTime);
+      }
+
+      accelerationWayBuffer = accelerationWay;
+
+      while (accelerationWayBuffer >= 0){
+          if(speed > startSpeed){
+            speed = speed - (acceleration * stepTime);
+            if(speed < startSpeed) speed = startSpeed;
+          }
+          stepTime = ()(1000000 / speed) / stepsPerMM) - impulseTime;
+          accelerationWayBuffer = accelerationWayBuffer - (speed * stepTime);
+          digitalWrite(stpPin, HIGH);
+          delayMicroseconds(impulseTime);
+          digitalWrite(stpPin, LOW);
+          delayMicroseconds(stepTime);
+      }
+
+      return position;
+  }
+
+  void  changePosition(float changePos){
+    this->stepsPerMM = (360.0 / anglePerStep) * stepDivision) / mmPerTurn;
+    this->position = poition + changePos;
+
+    if (changePos < 0){
+        if (reverseMod = false) digitalWrite(dirPin, HIGH);
+        if (reverseMod = true) digitalWrite(dirPin, LOW);
+
+        changePos = -changePos;
+        if (beginBorder != NONE && position < beginBorder){
+          changePos = changePos - (position - beginBorder);
+          this->position = beginBorder;
+        }
+    }
+    else{
+        if (reverseMod = false) digitalWrite(dirPin, LOW);
+        if (reverseMod = true) digitalWrite(dirPin, HIGH);
+
+        if (endBorder != NONE && position > endBorder){
+          changePos = changePos - (position - endBorder);
+          this->position = endBorder;
+        }
+    }
+
+      float accelerationWay = ((maxSpeed * maxSpeed) - (startSpeed * startSpeed)) / (2 * acceleration)
+      if (accelerationWay * 2 > changePos) accelerationWay = changePos / 2;
+
+      float accelerationWayBuffer = accelerationWay;
+      int stepTime = 0;
+      float speed = startSpeed;
+
+      while (accelerationWayBuffer >= 0){
+          if(speed < maxSpeed){
+            speed = speed + (acceleration * stepTime);
+            if(speed > maxSpeed) speed = maxSpeed;
+          }
+          stepTime = ()(1000000 / speed) / stepsPerMM) - impulseTime;
+          accelerationWayBuffer = accelerationWayBuffer - (speed * stepTime);
+          digitalWrite(stpPin, HIGH);
+          delayMicroseconds(impulseTime);
+          digitalWrite(stpPin, LOW);
+          delayMicroseconds(stepTime);
+      }
+
+      accelerationWayBuffer = changePos - (2 * accelerationWay);
+
+      while (accelerationWayBuffer >= 0){
+          accelerationWayBuffer = accelerationWayBuffer - (speed * stepTime);
+          digitalWrite(stpPin, HIGH);
+          delayMicroseconds(impulseTime);
+          digitalWrite(stpPin, LOW);
+          delayMicroseconds(stepTime);
+      }
+
+      accelerationWayBuffer = accelerationWay;
+
+      while (accelerationWayBuffer >= 0){
+          if(speed > startSpeed){
+            speed = speed - (acceleration * stepTime);
+            if(speed < startSpeed) speed = startSpeed;
+          }
+          stepTime = ()(1000000 / speed) / stepsPerMM) - impulseTime;
+          accelerationWayBuffer = accelerationWayBuffer - (speed * stepTime);
+          digitalWrite(stpPin, HIGH);
+          delayMicroseconds(impulseTime);
+          digitalWrite(stpPin, LOW);
+          delayMicroseconds(stepTime);
+      }
+  }
+
+  //End of position metods;
---
+++ src/Stepper.h
@@ -0,0 +1,76 @@
+#ifndef STEPPER_H
+#define STEPPER_H
+
+class Stepper{
+public:
+  //Constructor:
+  Stepper(byte stpPin, byte dirPin, byte enaPin = NONE);
+  //End of Constructor;
+
+
+  //Set settings:
+
+  //Settings of rotation for stepper motor:
+  void setmmPerTurn(float mmPerTurn);
+  void setAcceleration(int acceleration);
+  void setStepDivision(byte division);
+  void setAnglePerStep(float angle);
+  void setStartSpeed(float speed);
+  void setMaxSpeed(float speed);
+  //End of settings of rotation for stepper motor;
+
+  //Settings of operation mode:
+  void setMotorMode(String Mode, String enaLvl = "LOW");
+  void reverse();
+  void setImpulseTime(byte impulseTime);
+  //End of Settings of operation mode;
+
+  //Settings of position set:
+  void setPosition(float position = 0);
+  void setEndBorder(float endBorderPosition);
+  void setBeginBorder(float beginBorderPosition);
+  //End settings of position set;
+
+  //End of set-settings;
+
+
+  //Metods for work with stepper motor:
+
+  //Position metods:
+  void  position(float Position);
+  float changePosition(float changePos);
+  void  changePosition(float changePos);
+  //End of position metods;
+
+  //Information metods:
+  String showPosition(boolean ifshowPos = true, boolean ifshowBorders = true);
+  String showInformation(String nameOfStepperController, boolean ifshowAnglePerStep = true, boolean ifshowDivision = true);
+  //End of information metods;
+
+  //End of metods for work with stepper motor;
+
+private:
+  float anglePerStep = 1.8;
+  float stepsPerMM = 0.0;
+  float acceleration = 100.0; //mm/s^2
+  float maxSpeed = 60.0;// mm/s
+  float startSpeed = 5.0;// mm/s
+  byte impulseTime = 3;
+  byte stepDivision = 1;
+  //boolean motorMode = true;
+  boolean reverseMod = false;
+  float mmPerTurn = 10;
+
+  float position = 0;
+  float beginBorder = NONE;
+  float endBorder = NONE;
+
+  byte stpPin;
+  byte dirPin;
+  byte enaPin;
+
+}
+
+
+
+#endif STEPPER_H
